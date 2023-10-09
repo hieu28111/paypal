bb4005fe10c32791b5090ff336011c146a8cf691
NGUYỄN MINH HIẾU <130300574+hieu28111@users.noreply.github.com>
2023-10-09 21:34:40 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const ctn1 = document.querySelector(".container1");
+const caidat = document.querySelector(".caidat");
+const thoat = document.querySelector(".thoat");
+
+caidat.addEventListener("click", function () {
+  ctn1.classList.remove("hidden");
+  caidat.classList.add("hidden");
+});
+thoat.addEventListener("click", function () {
+  ctn1.classList.add("hidden");
+  caidat.classList.remove("hidden");
+});
+
+const ok = document.querySelector(".ok");
+const sodu = document.querySelector(".sodu");
+
+const send = document.querySelector(".send");
+
+ok.addEventListener("click", function () {
+  let nhapusd = Number(document.querySelector(".nhapusd").value);
+
+  sodu.textContent = nhapusd + ",00 $";
+});
+
+send.addEventListener("click", function () {
+  const x = Number(document.querySelector(".USD").value);
+  let nhapusd = Number(document.querySelector(".nhapusd").value);
+  const z = nhapusd - x;
+  nhapusd = document.querySelector(".nhapusd").value = z;
+
+  sodu.textContent = z + ",00 $";
+  Swal.fire("Send Money is Completed", "You sent " + x + ",00 $");
+});
